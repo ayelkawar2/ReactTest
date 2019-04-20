d2da4cf65367c7b8169169f91be9b214da6f2a45
ayelkawar2 <ayelkawar@gmail.com>
2019-04-19 19:43:22 -0700

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+import "./styles.css";
+
+class Test extends React.Component {
+  render() {
+    return <App />;
+  }
+}
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div>Test</div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+render(<Test />, rootElement);
