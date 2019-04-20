3d818cbbbfb360e41ce81ac7f938a4b3b973ffaa
ayelkawar2 <ayelkawar@gmail.com>
2019-04-19 20:11:27 -0700

    Test site initialization

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,7 @@ class Test extends React.Component {
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <div>Test Update again</div>
+      <h1>Aditya' Home</h1>
     </div>
   );
 }
